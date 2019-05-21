29879695aaffa97320a7404bb26f077fbcedea38
audreywelch <welch.audreyl@gmail.com>
2019-05-21 16:42:38 -0500

    Generated users and friendships in social.py

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,14 +1,19 @@
 
+# Undirected Graph
+# We are looking for connected components
+import random
 
 class User:
     def __init__(self, name):
         self.name = name
+    def __repr___(self):
+        return self.name
 
 class SocialGraph:
     def __init__(self):
         self.lastID = 0
-        self.users = {}
-        self.friendships = {}
+        self.users = {} # Vertices - indexed by ID
+        self.friendships = {} # Edges between users
 
     def addFriendship(self, userID, friendID):
         """
@@ -28,8 +33,9 @@ class SocialGraph:
         """
         self.lastID += 1  # automatically increment the ID to assign the new user
         self.users[self.lastID] = User(name)
-        self.friendships[self.lastID] = set()
+        self.friendships[self.lastID] = set() # Initialize with an empty set
 
+    # Seed our social network
     def populateGraph(self, numUsers, avgFriendships):
         """
         Takes a number of users and an average number of friendships
@@ -44,11 +50,27 @@ class SocialGraph:
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
-        # Add users
+        # Add users - we want to create `numUsers` number of users
+        for i in range(numUsers):
+            self.addUser(f"User {i + 1}")
 
         # Create friendships
+        # Generate every possible combination of friendships, shuffle it, then slice off exactly how many friendships we need to generate
+        possibleFriendships = []
+
+        # Generate all possible friendship combinations
+        for userID in self.users: # get all the keys in the users dictionary
+            for friendID in range(userID + 1, self.lastID + 1):
+                possibleFriendships.append((userID, friendID))
+
+        # Shuffle the friendship combinations
+        random.shuffle(possibleFriendships)
+
+        # Slice off exactly how many friendships we need to generate
+        for friendship_index in range(avgFriendships * numUsers // 2):
+            friendship = possibleFriendships[friendship_index]
+            self.addFriendship(friendship[0], friendship[1])
 
     def getAllSocialPaths(self, userID):
         """
@@ -67,6 +89,7 @@ class SocialGraph:
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populateGraph(10, 2)
+    print(sg.users)
     print(sg.friendships)
-    connections = sg.getAllSocialPaths(1)
-    print(connections)
+    # connections = sg.getAllSocialPaths(1)
+    # print(connections)
