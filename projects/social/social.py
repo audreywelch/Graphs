a0af00e3dce76f4f79992e218aedfcf1004d4989
audreywelch <welch.audreyl@gmail.com>
2019-05-22 15:38:17 -0500

    Fixed implementation of getting all friendship paths to also include friends of friends

--- projects/social/social.py
+++ projects/social/social.py
@@ -87,77 +87,44 @@ class SocialGraph:
             friendship = possibleFriendships[friendship_index]
             self.addFriendship(friendship[0], friendship[1])
 
+
     def getAllSocialPaths(self, userID):
         """
-        Takes a user's userID as an argument
-
-        Returns a dictionary containing every user in that user's
-        extended network with the shortest friendship path between them = BFS
-        
-        The key is the friend's ID and the value is the path.
-        
-        EX: {1: {2, 4, 7}} Node with nodes it connects to
-        Returns: {2: [1, 5, 2], 4: [None], 7: [1, 10, 2, 7]}
-
-        Execution:
-            - BFS (finds shortest path) for each node in the set of connections
-            - the path that is returned is inserted into the `visited` dictionary as the value to the key (userID)
-        
+        Plan:
+            - Do a BFS
+            - When we reach an unvisited user, append the path to the visited dictionary
+            - return visited
         """
-        # Create an empty queue
-        q = Queue()
-
-        # Create an empty set to store visited sets in order to keep track along the way
-        visited_set = set()
-
-        # Create an empty visited dictionary to store visited vertices and their paths
-        visited = {}  # Note that this is a dictionary, not a set
-
-        # Get the values/friendships stored with each user
-        all_friendships = self.friendships[userID]
-        print(f'{all_friendships}')
 
         # UserID is the starting vertex
         # Friendship node is the target vertex
 
-        # Enqueue a path that starts at the starting_vertex into the queue
-        q.enqueue( [userID] )
+        # Create an empty queue
+        q = Queue()
+
+        # Create an empty visited dictionary to store visited vertices and their paths
+        visited = {}  # Note that this is a dictionary, not a set
+
+        # Add a path to the starting vertex to the Queue
+        q.enqueue([userID])
 
         # While the queue is not empty...
         while q.size() > 0:
 
-            for each_friendship in self.friendships[userID]:
+            path = q.dequeue()
 
-                # Add the friendships to our final dictionary
-                visited.update({each_friendship:[]})
-                print(visited)
+            # Grab the last vertex/friend from the path
+            friend = path[-1]
 
-                #Dequeue the first path from the front of the array
-                path = q.dequeue()
+            # if the key is NOT in the dictinoary already
+            if friend not in visited:
+                # When we reach an unvisited user, append the path to the visited dictionary
+                visited[friend] = path
 
-                # Grab the last vertex of the path
-                v = path[-1]
-
-                # Check if v is the target
-                if v == each_friendship:
-                    # If v is the target AND the length of the path is less than the one currently in the dictionary:
-                    if len(path) < len(visited[each_friendship]):
-                        # Update the path in the dictionary
-                        visited.update(each_friendship = path)
-
-                # Check if in visited
-                if v not in visited_set:
-
-                    # Add the vertex to Visited
-                    visited_set.add(v)
-
-                    # Then enqueue each path to each of its neighbors in the queue
+                for neighbor in self.friendships[friend]:
                     path_copy = path.copy()
-                    path_copy.append(v)
+                    path_copy.append(neighbor)
                     q.enqueue(path_copy)
-                    print(visited_set)
-                    print(q)
-        
 
         return visited
 
@@ -165,7 +132,84 @@ class SocialGraph:
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populateGraph(10, 2)
-    print(sg.users)
+    #print(sg.users)
+    print("------------")
     print(sg.friendships)
-    connections = sg.getAllSocialPaths(2)
+    print("------------")
+    connections = sg.getAllSocialPaths(1)
     print(connections)
+
+
+    # def getAllSocialPathsFirstAttempt(self, userID):
+    #     """
+    #     Takes a user's userID as an argument
+
+    #     Returns a dictionary containing every user in that user's
+    #     extended network with the shortest friendship path between them = BFS
+        
+    #     The key is the friend's ID and the value is the path.
+        
+    #     EX: {1: {2, 4, 7}} Node with nodes it connects to
+    #     Returns: {2: [1, 5, 2], 4: [None], 7: [1, 10, 2, 7]}
+
+    #     Execution:
+    #         - BFS (finds shortest path) for each node in the set of connections
+    #         - the path that is returned is inserted into the `visited` dictionary as the value to the key (userID)
+        
+    #     """
+    #     # Create an empty queue
+    #     q = Queue()
+
+    #     # Create an empty set to store visited sets in order to keep track along the way
+    #     visited_set = set()
+
+    #     # Create an empty visited dictionary to store visited vertices and their paths
+    #     visited = {}  # Note that this is a dictionary, not a set
+
+    #     # Get the values/friendships stored with each user
+    #     all_friendships = self.friendships[userID]
+    #     print(f'{all_friendships}')
+
+    #     # UserID is the starting vertex
+    #     # Friendship node is the target vertex
+
+    #     # Enqueue a path that starts at the starting_vertex into the queue
+    #     q.enqueue( [userID] )
+
+    #     # While the queue is not empty...
+    #     while q.size() > 0:
+
+    #         for each_friendship in self.friendships[userID]:
+
+    #             # Add the friendships to our final dictionary
+    #             visited.update({each_friendship:[]})
+    #             print(visited)
+
+    #             #Dequeue the first path from the front of the array
+    #             path = q.dequeue()
+
+    #             # Grab the last vertex of the path
+    #             v = path[-1]
+
+    #             # Check if v is the target
+    #             if v == each_friendship:
+    #                 # If v is the target AND the length of the path is less than the one currently in the dictionary:
+    #                 if len(path) < len(visited[each_friendship]):
+    #                     # Update the path in the dictionary
+    #                     visited.update(each_friendship = path)
+
+    #             # Check if in visited
+    #             if v not in visited_set:
+
+    #                 # Add the vertex to Visited
+    #                 visited_set.add(v)
+
+    #                 # Then enqueue each path to each of its neighbors in the queue
+    #                 path_copy = path.copy()
+    #                 path_copy.append(v)
+    #                 q.enqueue(path_copy)
+    #                 print(visited_set)
+    #                 print(q)
+        
+
+    #     return visited
